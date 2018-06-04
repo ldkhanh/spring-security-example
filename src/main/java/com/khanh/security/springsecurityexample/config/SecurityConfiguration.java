12c99ae31388a72282a652f8b6daad92ba2634fd
khanhduyle <khanhduyle@assembla.com>
2018-06-04 03:00:35 -0700

    add

--- src/main/java/com/khanh/security/springsecurityexample/config/SecurityConfiguration.java
+++ src/main/java/com/khanh/security/springsecurityexample/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.khanh.security.springsecurityexample.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,13 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("khanh").password("test").roles("USER").and()
+                .withUser("diemvy").password("heongo").roles("ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
+                .antMatchers("*/hello").hasRole("USER")
                 //.antMatchers("**/rest/*")
-                .anyRequest()
-                .permitAll()
+                //.anyRequest()
+                //.fullyAuthenticated()
                 .and().httpBasic();
         httpSecurity.csrf().disable();
     }
